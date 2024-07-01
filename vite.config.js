2c04b16291f55c74371b4a0cfee21349341802dd
coumbass <sarelsowsn@gmail.com>
2024-07-01 01:39:42 +0000

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/projet_reactjs/",
+  base: "/react_project/",
   plugins: [react()],
 })
